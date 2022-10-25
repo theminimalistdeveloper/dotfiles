635b53a585bc22bb3b466fadb8c3734688f0a273
Rafael Dias Moreira <rafael.dias_moreira@seven.one>
2022-10-25 10:28:58 +0200

    chore: ui updates

--- nvim/.config/nvim/lua/ui.lua
+++ nvim/.config/nvim/lua/ui.lua
@@ -4,11 +4,14 @@
 
 -- Fix sign column (the column before the numbers) to avoid layout shifting
 vim.o.signcolumn = 'yes:1'
+-- Hide unused command line
+vim.o.ls = 0
+--vim.o.ch = 0
 -- Enables 24-bit RGB color in the |TUI|
 vim.o.termguicolors = true
 -- Set 4 lines on the edge of the screen
 vim.o.scrolloff = 4
--- Highlight cursorline
+-- Highlight cursor line
 vim.o.cursorline = true
 -- Show column markers at column 80 and 100
 vim.o.colorcolumn = 80
