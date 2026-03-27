2410860134701efe752f490f2746dcc067bd8590
Rafael Dias <rafael.dias_moreira@seven.one>
2026-03-27 15:04:00 +0100

    feat(neovim): Added joyia (prosieben specific) integration to CodeCompanion

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -3,24 +3,48 @@ vim.pack.add({
     'https://github.com/olimorris/codecompanion.nvim',
 })
 
+-- The name of the environment variable where you keep your Joyia API key
+local joyia_api_key = os.getenv('JOYIA_API_KEY')
+local joyia_api_url = os.getenv('JOYIA_API_URL')
+
+-- Definition of the custom JOYIA adapter
+local joyia_adapter = function()
+    local ok, adapters = pcall(require, 'codecompanion.adapters')
+    if not ok then
+        return nil
+    end
+
+    return adapters.extend('openai_compatible', {
+        name = 'joyia',
+        env = {
+            url = joyia_api_url,
+            api_key = joyia_api_key,
+        },
+        schema = {
+            model = {
+                default = 'claude-opus-4-6',
+            },
+        },
+    })
+end
+
+-- Personal adapter - Copilot
+local copilot_adapter = function()
+    return {
+        name = 'copilot',
+        model = 'gpt5-mini',
+    }
+end
+
+local adapter = (joyia_api_key and joyia_api_url) and joyia_adapter or copilot_adapter
+
 require('codecompanion').setup({
-    interactions = {
-        chat = {
-            adapter = {
-                name = 'copilot',
-                model = 'gpt-5-mini',
-            },
-        },
-        inline = {
-            adapter = {
-                name = 'copilot',
-                model = 'gpt-5-mini',
-            },
-        },
+    strategies = {
+        chat = { adapter = adapter },
+        inline = { adapter = adapter },
     },
 })
 
--- Keybindings
 vim.keymap.set('n', '<leader>aa', '<cmd>CodeCompanionActions<cr>', { desc = 'CodeCompanion Actions' })
 vim.keymap.set('v', '<leader>aa', '<cmd>CodeCompanionActions<cr>', { desc = 'CodeCompanion Actions' })
 vim.keymap.set('n', '<leader>ac', '<cmd>CodeCompanionChat Toggle<cr>', { desc = 'Toggle CodeCompanion Chat' })
