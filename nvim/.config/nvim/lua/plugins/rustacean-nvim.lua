9ea0e21c2f6da80bc6a3d40a951317f94843a821
Rafael Dias <rafael.dias_moreira@seven.one>
2026-04-25 15:15:12 +0200

    chore(neovim): improve rustacean config

--- nvim/.config/nvim/lua/plugins/rustacean-nvim.lua
+++ nvim/.config/nvim/lua/plugins/rustacean-nvim.lua
@@ -2,22 +2,23 @@ vim.pack.add({
     'https://github.com/mrcjkb/rustaceanvim',
 })
 
-local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set(
-  "n",
-  "<leader>c",
-  function()
-    vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
-    -- or vim.lsp.buf.codeAction() if you don't want grouping.
-  end,
-  { silent = true, buffer = bufnr }
-)
+-- vim.g.rustaceanvim.tools.code_actions.ui_select_fallback = true
 
-vim.keymap.set(
-  "n",
-  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-  function()
-    vim.cmd.RustLsp({'hover', 'actions'})
-  end,
-  { silent = true, buffer = bufnr }
-)
+-- RUST $U
+vim.keymap.set('n', '<leader>ur', '<cmd>RustLsp run<cr>')
+vim.keymap.set('n', '<leader>ud', '<cmd>RustLsp openDocs<cr>')
+vim.keymap.set('n', '<leader>ua', '<cmd>RustLsp hover actions<cr>')
+vim.keymap.set('n', '<leader>un', '<cmd>RustLsp hover range<cr>')
+vim.keymap.set('n', '<leader>uu', '<cmd>RustLsp debbugables<cr>')
+vim.keymap.set('n', '<leader>ue', '<cmd>RustLsp expandMacro<cr>')
+vim.keymap.set('n', '<leader>uc', '<cmd>RustLsp codeAction<cr>')
+vim.keymap.set('n', '<leader>ux', '<cmd>RustLsp explainError current<cr>')
+vim.keymap.set('n', '<leader>umu', '<cmd>RustLsp moveItem up<cr>')
+vim.keymap.set('n', '<leader>umd', '<cmd>RustLsp moveItem down<cr>')
+vim.keymap.set('n', '<leader>ui', '<cmd>RustLsp renderDiagnostic<cr>')
+vim.keymap.set('n', '<leader>ut', '<cmd>RustLsp relatedTests<cr>')
+vim.keymap.set('n', '<leader>uo', '<cmd>RustLsp openCargo<cr>')
+vim.keymap.set('n', '<leader>uj', '<cmd>RustLsp joinLines<cr>')
+vim.keymap.set('n', '<leader>ug', '<cmd>RustLsp crateGraph<cr>')
+vim.keymap.set('n', '<leader>uvh', '<cmd>RustLsp view hir<cr>')
+vim.keymap.set('n', '<leader>uvm', '<cmd>RustLsp view mir<cr>')
