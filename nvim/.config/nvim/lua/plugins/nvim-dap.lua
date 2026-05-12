58a5a32154acc6f26fffba17570a3725ab0b874f
Rafael Dias <rafael.dias_moreira@seven.one>
2026-05-12 15:12:45 +0200

    feat: added nvim-dap

---
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,112 @@
+vim.pack.add({
+    'https://github.com/mfussenegger/nvim-dap',
+    'https://github.com/rcarriga/nvim-dap-ui',
+    'https://github.com/nvim-neotest/nvim-nio',
+    'https://github.com/theHamsta/nvim-dap-virtual-text',
+    -- JS/TS debugger
+    'https://github.com/mxsdev/nvim-dap-vscode-js',
+    'https://github.com/microsoft/vscode-js-debug',
+})
+
+local dap = require('dap')
+local dapui = require('dapui')
+
+dap.set_log_level('TRACE')
+-------------------------------------------------------------------------------
+-- DAP UI
+-------------------------------------------------------------------------------
+dapui.setup()
+
+require('nvim-dap-virtual-text').setup()
+
+-- Automatically open/close DAP UI when debugging starts/ends
+dap.listeners.after.event_initialized['dapui_config'] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated['dapui_config'] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited['dapui_config'] = function()
+    dapui.close()
+end
+
+-------------------------------------------------------------------------------
+-- SIGNS
+-------------------------------------------------------------------------------
+vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointCondition', { text = '◐', texthl = 'DapBreakpointCondition', linehl = '', numhl = '' })
+vim.fn.sign_define('DapLogPoint', { text = '◆', texthl = 'DapLogPoint', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '▶', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+vim.fn.sign_define('DapBreakpointRejected', { text = '○', texthl = 'DapBreakpointRejected', linehl = '', numhl = '' })
+
+-------------------------------------------------------------------------------
+-- NODE / TYPESCRIPT (vscode-js-debug)
+-------------------------------------------------------------------------------
+dap.adapters['pwa-node'] = {
+  type = 'server',
+  host = 'localhost',
+  port = '${port}',
+  executable = {
+    command = 'node',
+    args = {'/Users/dia0001r/code/lab/js-debug/src/dapDebugServer.js', '${port}'}
+  }
+}
+
+dap.configurations.javascript = {
+   {
+     type = 'pwa-node',
+     request = 'attach',
+     name = 'Attach to Node app',
+     address = 'localhost',
+     port = 9229,
+     cwd = '${workspaceFolder}',
+     restart = true,
+   },
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+}
+dap.configurations.typescript = {
+   {
+     type = 'pwa-node',
+     request = 'attach',
+     name = 'Attach to Node app',
+     address = 'localhost',
+     port = 9230,
+     cwd = '${workspaceFolder}',
+     restart = true,
+   },
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+}
+
+-------------------------------------------------------------------------------
+-- KEYMAPS
+-------------------------------------------------------------------------------
+vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Toggle breakpoint' })
+vim.keymap.set('n', '<leader>dB', function()
+    dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
+end, { desc = 'Set conditional breakpoint' })
+vim.keymap.set('n', '<leader>dl', function()
+    dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+end, { desc = 'Set log point' })
+vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Continue' })
+vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Step into' })
+vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'Step over' })
+vim.keymap.set('n', '<leader>dO', dap.step_out, { desc = 'Step out' })
+vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = 'Open REPL' })
+vim.keymap.set('n', '<leader>dR', dap.run_last, { desc = 'Run last' })
+vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = 'Toggle DAP UI' })
+vim.keymap.set('n', '<leader>dx', dap.terminate, { desc = 'Terminate' })
+vim.keymap.set({ 'n', 'v' }, '<leader>de', function()
+    dapui.eval(nil, { enter = true })
+end, { desc = 'Eval under cursor' })
