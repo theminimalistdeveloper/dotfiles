54568bebd55dc99f8b2fc8bb4733a31894a073bc
Rafael Dias <rafael.dias_moreira@seven.one>
2026-06-24 08:47:32 +0200

    chore(nvim): updated Markview config - support telekasten - support codecompanion

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,8 +1,12 @@
 vim.pack.add({'https://github.com/OXY2DEV/markview.nvim'})
 
+vim.treesitter.language.register('markdown', 'telekasten')
+vim.treesitter.language.register('markdown', 'codecompanion')
+
 require('markview').setup({
   preview = {
-    filetypes = { "markdown", "codecompanion" },
-    ignore_buftypes = {},
+    icon_provider = 'internal',
+    filetypes = { 'markdown', 'telekasten', 'codecompanion' },
+    ignore_buftypes = {}
   }
 })
