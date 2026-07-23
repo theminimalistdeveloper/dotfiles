dc804ce692dc00de3c6512947694ef9f5f70d45e
Rafael Dias <rafael.dias_moreira@seven.one>
2026-07-23 09:36:28 +0200

    chore(neovim): moved to tiny model

--- nvim/.config/nvim/lua/plugins/whisper.lua
+++ nvim/.config/nvim/lua/plugins/whisper.lua
@@ -2,12 +2,10 @@ return {
   'Avi-D-coder/whisper.nvim',
   config = function()
     require('whisper').setup({
-      model = 'base.en',
-      keybind = '<c-g>',
-      manual_trigger_key = '<Enter>',
+      model = 'tiny.en',
+      on_transcription = function(text)
+        vim.notify("Transcribed: " .. text)
+      end,
     })
   end,
-  keys = {
-    { '<c-g>', mode = {'n','i', 'v'}, desc = 'Toggle speech-to-text' }
-  },
 }
